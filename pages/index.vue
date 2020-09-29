990c03f15968b6e723162674c9c4d24cf7a08e41
Pablo Miceli <d1urno@gmx.com>
2020-09-29 14:55:13 -0300

    feat: create home page
    - Load and show offices

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="bg-gray-100">
+		<section class="container flex flex-col items-center px-6 pt-16 mx-auto">
+			<h1 class="mb-6 text-6xl font-light tracking-wide text-teal-500">
+				Offices
+			</h1>
+
+			<button class="block w-full max-w-md px-8 py-5 my-6
+				text-left text-white bg-red-500 rounded-lg shadow-lg
+				focus:outline-none">
+				Add New location
+			</button>
+
+			<ul class="w-full mb-8 space-y-6">
+				<li v-for="location in _locations" :key="location.id" class="w-full max-w-md mx-auto">
+					<office-card :location="location"/>
+				</li>
+			</ul>
+		</section>
+
+		<footer class="container px-6 pb-16 mx-auto text-center">
+			<p class="mb-1 text-gray-500">
+				This project is for test purpose only.
+			</p>
+			<a class="text-xs tracking-wider text-teal-500 focus:outline-none focus:underline"
+			   href="https://dogandponystudios.com"
+			   rel="nofollow">
+				WWW.DOGANDPONYSTUDIOS.COM
+			</a>
+		</footer>
+	</div>
+</template>
+
+<script>
+import OfficeCard from '@/components/OfficeCard'
+
+export default {
+	components: {OfficeCard},
+	computed: {
+		_locations() {
+			return this.$store.getters.locations
+		}
+	}
+}
+</script>
