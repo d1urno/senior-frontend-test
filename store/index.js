97e3fcc8aa5bb83394cd098ec302e506c10f2fd5
Pablo Miceli <d1urno@gmx.com>
2020-09-29 11:11:21 -0300

    feat: create store
    - Use sample data file

---
+++ store/index.js
@@ -0,0 +1,49 @@
+import data from "@/assets/data.json"
+
+export const state = () => ({
+	locations: [...data]
+})
+
+export const mutations = {
+	ADD_LOCATION(state, location) {
+		state.locations.unshift(location)
+	},
+	EDIT_LOCATION(state, location) {
+		state.locations = state.locations.map((value) => {
+			if (value.id === location.id) return location
+			return value;
+		});
+	},
+	DELETE_LOCATION(state, locationId) {
+		state.locations.splice(locationId, 1)
+	}
+}
+
+export const actions = {
+	addLocation({commit}, location) {
+		commit('ADD_LOCATION', location)
+		return location
+	},
+	editLocation({commit}, location) {
+		commit('EDIT_LOCATION', location)
+		return location
+	},
+	deleteLocation({state, commit}, location) {
+		const index = state.locations
+			.map((value) => {
+				return location.id === value.id
+			})
+			.indexOf(true)
+
+		if (index !== -1) {
+			commit('DELETE_LOCATION', index)
+			return location
+		} else {
+			return false
+		}
+	}
+}
+
+export const getters = {
+	locations: (state) => state.locations
+}
