177189bfacb72560b723fafaa5adcfe73bd4be6f
Pablo Miceli <d1urno@gmx.com>
2020-09-29 15:46:38 -0300

    feat(OfficeCard): apply vue-smooth-reflow
    - Improve transition
    
    - Reformat code

--- components/OfficeCard.vue
+++ components/OfficeCard.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="bg-white rounded-b-lg shadow-lg">
+	<div class="relative overflow-y-hidden bg-white rounded-b-lg shadow-lg">
 		<header class="relative z-10">
 			<button @click="isOpen = !isOpen" :class="_colorClass" class="h-48 p-8 w-full
 				flex flex-col justify-center text-white rounded-lg shadow-lg
@@ -9,34 +9,36 @@
 			</button>
 		</header>
 
-		<transition name="slide-top">
-			<div v-if="isOpen" class="px-12">
-				<section class="pt-8 pb-4 -mt-2 space-y-3">
-					<h3 class="block text-3xl font-semibold text-gray-800">
-						{{ location.contact.name }}
-					</h3>
-					<p>{{ location.contact.title }}</p>
-					<a :href="`mailto:${location.contact.email}`" class="text-teal-500 inline-block
-				focus:outline-none focus:underline">
-						{{ location.contact.email }}
-					</a>
-					<p>{{ location.contact.phone }}</p>
-				</section>
+		<smooth-reflow>
+			<transition name="slide-top">
+				<div v-if="isOpen" class="px-12">
+					<section class="pt-8 pb-4 -mt-2 space-y-3">
+						<h3 class="block text-3xl font-semibold text-gray-800">
+							{{ location.contact.name }}
+						</h3>
+						<p>{{ location.contact.title }}</p>
+						<a :href="`mailto:${location.contact.email}`"
+						   class="inline-block text-teal-500 focus:outline-none focus:underline">
+							{{ location.contact.email }}
+						</a>
+						<p>{{ location.contact.phone }}</p>
+					</section>
 
-				<span class="block w-full h-1 border-t border-gray-300"/>
+					<span class="block w-full h-1 border-t border-gray-300"/>
 
-				<footer class="flex justify-between px-1 pt-4 pb-6">
-					<button class="px-2 py-1 -mx-2 -my-1 text-xs text-gray-500 rounded-lg hover:bg-gray-100
-						focus:outline-none focus:bg-gray-100 tracking-wider">
-						EDIT
-					</button>
-					<button class="px-2 py-1 -mx-2 -my-1 text-xs text-red-500 rounded-lg hover:bg-red-100
-						focus:outline-none focus:bg-red-100 tracking-wider">
-						DELETE
-					</button>
-				</footer>
-			</div>
-		</transition>
+					<footer class="flex justify-between px-1 pt-4 pb-6">
+						<button class="px-2 py-1 -mx-2 -my-1 text-xs text-gray-500 rounded-lg hover:bg-gray-100
+							focus:outline-none focus:bg-gray-100 tracking-wider">
+							EDIT
+						</button>
+						<button class="px-2 py-1 -mx-2 -my-1 text-xs text-red-500 rounded-lg hover:bg-red-100
+							focus:outline-none focus:bg-red-100 tracking-wider">
+							DELETE
+						</button>
+					</footer>
+				</div>
+			</transition>
+		</smooth-reflow>
 	</div>
 </template>
 
@@ -73,11 +75,14 @@ export default {
 <style>
 .slide-top-enter,
 .slide-top-leave-to {
-	@apply transform opacity-0;
+	@apply transform opacity-0
 }
 
-.slide-top-leave-active,
+.slide-top-leave-active {
+	@apply transition duration-500 absolute inset-x-0
+}
+
 .slide-top-enter-active {
-	@apply transition duration-500;
+	@apply transition duration-500
 }
 </style>
