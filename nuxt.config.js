45990267b8574f784c7a005991e39a19c6ae816d
Pablo Miceli <d1urno@gmx.com>
2020-10-01 20:07:45 -0300

    feat(nuxt): add lang meta attribute

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 	 * Headers of the page
 	 */
 	head: {
+		htmlAttrs: {
+			lang: 'en'
+		},
 		title: process.env.npm_package_name || 'Dog and Pony Studios - Senior Frontend Developer Test',
 		meta: [
 			{ charset: 'utf-8' },
